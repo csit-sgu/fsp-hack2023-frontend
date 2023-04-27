97443335471951541caca5dbbdac873e0f09d106
Michael Chernigin <michaelchernigin@gmail.com>
2023-04-27 20:30:11 +0300

    Кнопка регистрации

--- src/components/LoginBox.vue
+++ src/components/LoginBox.vue
@@ -1,23 +1,27 @@
+<script setup lang="ts">
+import BackgroundSquares from '../components/BackgroundSquares.vue';
+</script>
+
 <template>
   <div class="flex items-center justify-center h-screen relative isolate px-6 pt-14 lg:px-8 md:text-center">
+    <BackgroundSquares />
     <div class="w-full max-w-xs">
-      <form class="bg-white rounded px-8 pt-6 pb-8 mb-4">
+      <form class="bg-white backdrop border rounded-lg px-8 pt-6 pb-8 mb-4">
         <div class="mb-4">
           <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="username">
             Электронная почта
           </label>
           <input
-            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-            id="username" type="text" placeholder="Электронная почта">
+            class="appearance-none block w-full bg-gray-200 text-gray-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            id="username" type="text" placeholder="bob@aboba.com">
         </div>
         <div class="mb-6">
           <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="password">
             Пароль
           </label>
           <input
-            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            class="appearance-none block w-full bg-gray-200 text-gray-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
             id="password" type="password" placeholder="******************">
-          <p class="text-red-500 text-xs italic">Пароль должен быть не пустым</p>
         </div>
         <div class="flex items-center justify-between">
           <button
