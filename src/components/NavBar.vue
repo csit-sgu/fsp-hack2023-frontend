54c824086d1b9dc0e5f4fe7c20d69d5787917b78
Michael Chernigin <michaelchernigin@gmail.com>
2023-04-27 12:16:53 +0300

    NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+    <!-- FIX: width overflow -->
+    <b-navbar fixed-top=true >
+        <template #brand>
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <img src="@/assets/images/logo.png">
+            </b-navbar-item>
+        </template>
+        <template #start>
+            <b-navbar-item href="#">
+                Главная
+            </b-navbar-item>
+            <b-navbar-item href="#/competitions">
+                Соревнования
+            </b-navbar-item>
+            <b-navbar-dropdown label="Info">
+                <b-navbar-item href="#">
+                    About
+                </b-navbar-item>
+                <b-navbar-item href="#">
+                    Contact
+                </b-navbar-item>
+            </b-navbar-dropdown>
+        </template>
+
+        <template #end>
+            <b-navbar-item tag="div">
+                <div class="buttons">
+                    <a class="button is-primary">
+                        <strong>Зарегистрироваться</strong>
+                    </a>
+                    <a class="button is-light">
+                        Войти
+                    </a>
+                </div>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+</template>
+
+<script lang="ts">
+  export default {};
+</script>
