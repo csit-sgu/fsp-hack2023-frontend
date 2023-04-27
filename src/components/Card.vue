c56daa2fa6d9349c6d236a8d9ac144a56b7aaba5
Andrew Guschin <guschin.drew@gmail.com>
2023-04-27 15:42:24 +0300

    Добавлен компонент Card

---
+++ src/components/Card.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="flex-none rounded-2xl bg-neutral-100 w-[360px] h-[430px]">
+    <!-- <a href="#">
+      <img class="card__image" alt="" />
+    </a> -->
+    <div class="p-[25px] flex flex-col">
+      <div class="mb-2 text-xl font-medium leading-tight text-neutral-900 dark:text-neutral-50">
+        Хакатон в рамках Всероссийских соревнований по спортивному программированию
+      </div>
+      <div class="text-sm p-[10px]">
+        <span class="text-[#FF9533]">Дата проведения:</span>
+        <div class="grid grid-cols-2 gap-x-[10px]">
+          <span class="text-[#FF9533] ">Начало:</span> <span>27 апреля 2023</span>
+          <span class="text-[#FF9533]">Конец:</span> <span>27 апреля 2023</span>
+        </div>
+      </div>
+      <p class="mb-4 text-sm text-neutral-600 dark:text-neutral-200 h-[125px] overflow-hidden">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut porta dolor
+        ac bibendum aliquet. Quisque nec tortor vel purus vulputate blandit ut
+        et tellus. Maecenas euismod, ante non bibendum commodo, ligula dui
+        vehicula mauris, aliquet auctor orci velit sit amet quam. Suspendisse
+        pulvinar sed est vel blandit. Etiam porttitor ligula sit amet ligula
+        ullamcorper sollicitudin.
+      </p>
+      <div class="flex justify-center">
+        <button type="button"
+          class="inline-block w-fit rounded-lg bg-[#FF9533] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white">
+          Открыть подробности
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+/* img {
+  height: auto;
+  max-width: 100%;
+  vertical-align: middle;
+} */
+.card__image {
+  background-image: url("https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg");
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  border-top-left-radius: 0.25rem;
+  border-top-right-radius: 0.25rem;
+  filter: contrast(75%);
+  overflow: hidden;
+  position: relative;
+  transition: filter 0.5s cubic-bezier(0.43, 0.41, 0.22, 0.91);
+}
+
+/* .card__content {
+  display: flex;
+  flex: 1 1 auto;
+  flex-direction: column;
+  padding: 1rem;
+} */
+
+/* .card__image::before {
+  content: "";
+  display: block;
+  padding-top: 56.25%;
+} */
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import HomeLoggedView from '../views/HomeLoggedView.vue'
 
 Vue.use(VueRouter)
 
@@ -16,10 +17,12 @@ const router = new VueRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/main-logged',
+      name: 'main-logged',
+      component: HomeLoggedView
     }
   ]
 })
